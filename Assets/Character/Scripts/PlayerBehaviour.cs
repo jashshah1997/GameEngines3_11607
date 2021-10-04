a6806edac80afec2e06fc231ff7607115f003bc7
predator1245 <jashshah37@yahoo.com>
2021-10-04 10:13:21 +0200

    Add random encounters

--- Assets/Character/Scripts/PlayerBehaviour.cs
+++ Assets/Character/Scripts/PlayerBehaviour.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed = 1.0f;
+    private static float x_pos = 4;
+    private static float y_pos = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        gameObject.transform.position = new Vector3(x_pos, y_pos, 0);
     }
 
     // Update is called once per frame
@@ -19,5 +22,18 @@ public class PlayerBehaviour : MonoBehaviour
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(inputX * moveSpeed, inputY * moveSpeed, 0);
+        x_pos = gameObject.transform.position.x;
+        y_pos = gameObject.transform.position.y;
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Bush")
+        {
+            if (Random.Range(0, 250) < 2)
+            {
+                SceneManager.LoadScene("FightScene");
+            }
+        }
     }
 }
