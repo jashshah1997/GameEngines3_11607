83c101c6b5451a89b7f5a6e1798d04512fdcc12d
predator1245 <jashshah37@yahoo.com>
2021-11-03 21:10:49 +0100

    Add player animations

--- Assets/Character/Scripts/PlayerBehaviour.cs
+++ Assets/Character/Scripts/PlayerBehaviour.cs
@@ -10,6 +10,7 @@ public class PlayerBehaviour : MonoBehaviour
     private static float x_pos = 4;
     private static float y_pos = 0;
     private static bool firstRun = true;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,7 @@ public class PlayerBehaviour : MonoBehaviour
         if (!firstRun)
             gameObject.transform.position = new Vector3(x_pos, y_pos, 0);
         firstRun = false;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,6 +26,34 @@ public class PlayerBehaviour : MonoBehaviour
     {
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
+
+        animator.ResetTrigger("FrontTrigger");
+        animator.ResetTrigger("BackTrigger");
+        animator.ResetTrigger("LeftTrigger");
+        animator.ResetTrigger("RightTrigger");
+        animator.ResetTrigger("IdleTrigger");
+
+        if (inputX > 0)
+        {
+            animator.SetTrigger("RightTrigger");
+        }
+        else if (inputX < -0)
+        {
+            animator.SetTrigger("LeftTrigger");
+        }
+        else if (inputY > 0)
+        {
+            animator.SetTrigger("BackTrigger");
+        }
+        else if (inputY < -0)
+        {
+            animator.SetTrigger("FrontTrigger");
+        }
+        else
+        {
+            animator.SetTrigger("IdleTrigger");
+        }
+
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(inputX * moveSpeed, inputY * moveSpeed, 0);
         x_pos = gameObject.transform.position.x;
         y_pos = gameObject.transform.position.y;
